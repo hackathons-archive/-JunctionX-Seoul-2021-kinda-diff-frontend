adeacadff62ff2e554acc3b7d5d00e592313d347
Hubertus Setiawan <hubertus.setiawan@pricehubble.com>
2021-05-22 20:16:35 +0900

    pocker table

--- kindadiff-frontend/src/components/Game.vue
+++ kindadiff-frontend/src/components/Game.vue
@@ -1,40 +1,98 @@
 <template>
-<div>
-  <md-menu md-direction="top-end">
-    <md-button>Invite</md-button>
-    <md-button class="md-accent">Help</md-button>
-    <md-button class="md-primary">Leave</md-button>
+<div class = "back-color">
 
-  </md-menu>
-<div>
-  This is the board game
+<div class="text">
+    <v-btn class="grey-button" style="color: white; margin-right: 10px" rounded color="#9A9A9A">
+     invite
+    </v-btn>
+    <v-btn class="grey-button" style="color: white; margin-right: 10px" rounded color="#9A9A9A">
+      help
+    </v-btn>
+    <v-btn class="grey-button" style="color: white; margin-right: 10px" rounded color="#9A9A9A">
+    leave
+    </v-btn>
+
+    <v-card
+        style="margin-left:20px;"
+        max-width="344"
+        max-height="100"
+    >
+      <v-card-text>
+       <div class="text-center">Invite Code: abc123</div>
+      </v-card-text>
+      <v-card-actions>
+
+      </v-card-actions>
+    </v-card>
+
+
 </div>
-  <div class="table">
-  </div>
+
+  <img class="pokertable" src="../image/table.svg" alt="tablepocker" height="600" style="margin-top: 20px; margin-left: -300px; text-align: center; position: absolute;">
+  <img src="../image/profile.svg" alt="tablepocker" height="437" style="margin-top: -60px; margin-left:30px; position: relative;">
+
+  <v-footer
+      color="#F8981D"
+      height="110px"
+      padless
+      class="footer-card"
+  >
+    <v-row
+        justify="center"
+        no-gutters
+    >
+
+      <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+      >
+        <v-btn
+            rounded
+            color="white"
+            style="color: #F8981D; font-size: 30px"
+            class="bottom-button"
+            height="60px"
+
+        >
+        START
+        </v-btn>      </v-col>
+    </v-row>
+  </v-footer>
 </div>
 </template>
 
 <script>
 export default {
-name: "Game"
+name: "Game",
+  data: () => ({
+    newPosts: 0,
+    checkInterval: null
+  }),
+
 }
 </script>
 
 <style scoped>
 
-.table{
-  width:700px;
-  height:400px;
-  background-color:#4aad4a;
-  left:50%;
-  top:80%;
-  margin-top: 290px;
-  transform:translateX(-50%) translateY(-50%);
-  border-radius:150px;
-  position:relative;
-  border:10px solid #a95555;
+.text{
+ text-align: right;
 }
 
+.footer-card{
+  margin-top: 350px;
+}
+
+.back-color{
+  background-color: #FFFBF2;
+  margin-bottom: 0px;
+  margin-top: -55px;
+
+}
+
+
+.bottom-button{
+  margin-bottom: 10px;
+}
 
 
 </style>
--- kindadiff-frontend/src/main.js
+++ kindadiff-frontend/src/main.js
@@ -1,17 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import router from "./router/index.js";
-import { MdButton, MdContent, MdTabs, MdChips} from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
-Vue.use(MdButton);
-Vue.use(MdContent);
-Vue.use(MdTabs);
-Vue.use(MdChips);
+Vue.use(Vuetify);
 
-
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        orange: "#F8981D",
+      }
+    }
+  }
+});
 
 
 new Vue({
